5e84ff2f8ee7f6237b1115957c3b778e62a0aae6
ajz7em <128267683+ajz7em@users.noreply.github.com>
2025-09-17 15:05:06 -0400

    ci: install playwright browsers

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const config = {
-  experimental: {
-    serverActions: true,
-  },
   reactStrictMode: true,
   eslint: {
     dirs: ['app', 'components', 'lib'],
